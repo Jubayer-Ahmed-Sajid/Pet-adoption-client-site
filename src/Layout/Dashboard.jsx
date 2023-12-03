570c7c25b1786f9f185b797a7bb8e477a2ae4c8c
Jubayer-Ahmed-Sajid <jubayerahmed661ac@gmail.com>
2023-12-03 17:28:42 +0600

    banner section completed

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -12,79 +12,80 @@ const DashBoard = () => {
 
     return (
         <div className="flex">
-            <div className="w-60 bg-orange-400 min-h-screen">
+            <div className="w-60 bg-slate-400 min-h-screen">
                 <ul className="menu p-4 space-y-4">
                     {
                         isAdmin ? <>
                             <li>
-                                <NavLink to='/dashboard'>
-                                  <span  className="flex items-center gap-2"> <h2> <FaHome></FaHome></h2> <p>Admin Home</p> </span>
+                                <NavLink to='/dashboard' className='flex items-center gap-2'>
+                                  <span  className="flex items-center gap-2"> <h2> <FaHome className="text-2xl"></FaHome></h2> <p>Admin Home</p> </span>
                                 </NavLink>
                             </li>
                             <li>
-                                <NavLink to='/dashboard/allusers'>
-                                    <FaUtensils></FaUtensils> All Users
+                                <NavLink to='/dashboard/allusers' className='flex items-center gap-2'>
+                                    <FaUtensils className="text-2xl"></FaUtensils> All Users
                                 </NavLink>
                             </li>
                             <li>
-                                <NavLink to="/dashboard/allpets">
-                                    <FaBook></FaBook> All Pets
+                                <NavLink to="/dashboard/allpets" className='flex items-center gap-2'>
+                                    <FaBook className="text-2xl"></FaBook> All Pets
                                 </NavLink>
                             </li>
                             <li>
-                                <NavLink to="/dashboard/alldonations">
-                                    <FaUsers></FaUsers>All Donations
+                                <NavLink to="/dashboard/alldonations" className='flex items-center gap-2'>
+                                    <FaUsers className="text-2xl"></FaUsers>All Donations
                                 </NavLink>
                             </li>
 
                         </> : <>
                             <li>
-                                <NavLink to='/dashboard'>
-                                    <FaHome></FaHome> User Home
+                                <NavLink to='/dashboard' className='flex items-center gap-2'>
+                                    <FaHome className="text-2xl"></FaHome> User Home
                                 </NavLink>
                             </li>
                             <li>
-                                <NavLink to='/dashboard/addpet'>
-                                    <FaCalendar></FaCalendar> Add a Pet
+                                <NavLink to='/dashboard/addpet' className='flex items-center gap-2'>
+                                    <FaCalendar className="text-2xl"></FaCalendar> Add a Pet
                                 </NavLink>
                             </li>
                             <li>
-                                <NavLink to="/dashboard/addedpets">
-                                    <FaShoppingCart></FaShoppingCart> My Added Pets
+                                <NavLink to="/dashboard/addedpets" className='flex items-center gap-2'>
+                                    <FaShoppingCart className="text-2xl"></FaShoppingCart> My Added Pets
                                 </NavLink>
                             </li>
                             <li>
-                                <NavLink to="/dashboard/adoptionrequest">
-                                    <FaAd></FaAd>Adoption Request
+                                <NavLink to="/dashboard/adoptionrequest" className='flex items-center gap-2'>
+                                    <FaAd className="text-2xl"></FaAd>Adoption Request
                                 </NavLink>
                             </li>
                             <li>
-                                <NavLink to="/dashboard/createdonation">
-                                    <FaList></FaList> Create Donation Campaign
+                                <NavLink to="/dashboard/createdonation" className='flex items-center gap-2'>
+                                    <FaList className="text-2xl"></FaList> Create Donation Campaign
                                 </NavLink>
                             </li>
                             <li>
-                                <NavLink to="/dashboard/createddonation">
-                                    <FaList></FaList>My Created Donation Campaign
+                                <NavLink to="/dashboard/createddonation" className='flex items-center gap-2'>
+                                    <FaList className="text-2xl"></FaList>My Created Donation Campaign
                                 </NavLink>
                             </li>
                             <li>
-                                <NavLink to="/dashboard/mydonation">
-                                    <FaList></FaList> My Donation
+                                <NavLink to="/dashboard/mydonation" className='flex items-center gap-2'>
+                                    <FaList className="text-2xl"></FaList> My Donation
                                 </NavLink>
                             </li>
                         </>
                     }
                     <div className="divider"></div>
                     {/* Shared Routes */}
+                    <hr />
                     <li>
-                        <NavLink to='/'>
-                            <FaHome></FaHome>  Home
+                        <NavLink to='/' className='flex items-center gap-2'>
+                            <FaHome className="text-2xl"></FaHome>  Home
                         </NavLink>
                     </li>
                     <li>
-                        <NavLink to='/pets'>
-                            <FaSearch></FaSearch>  Pets
+                        <NavLink to='/pets' className='flex items-center gap-2'>
+                            <FaSearch className="text-2xl"></FaSearch>  Pets
                         </NavLink>
                     </li>
                 </ul>
@@ -96,3 +97,19 @@ const DashBoard = () => {
 };
 
 export default DashBoard;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -1,12 +1,13 @@
-import  {Swiper,SwiperSlide} from "swiper/react";
+import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
+import { Link } from "react-router-dom";
 const Banner = () => {
-    return (
-        <div className="relative mt-20">
-            <Swiper
+  return (
+    <div className="relative mt-20">
+      <Swiper
         spaceBetween={30}
         centeredSlides={true}
         autoplay={{
@@ -21,22 +22,36 @@ const Banner = () => {
         className="mySwiper"
       >
         <SwiperSlide className="relative">
-            <h2 className="absolute left-24 text-4xl  text-white top-32 ">Hello world</h2>
-            <img className="w-full h-[80vh] object-cover" src="https://i.ibb.co/10pq6F2/images-1.jpg" alt="" />
+          <div className="absolute lg:left-24 text-center lg:top-32  ">
+            <h2 className=" text-4xl font-bold mb-4 text-white "> Add a New Pet To Your Family  <br /> <span className="text-red-400 pt-4">...A Pet</span> </h2>
+            <p className=" text-xl font-semibold text-white "> Find joy, adopt a furry friend  today! <br /> Explore  our  loving pets waiting for <br /> their forever homes. Adopt, don't shop!</p>
+            <button className="mt-6 py-2 px-3 rounded-lg bg-slate-400 text-xl font-semibold "><Link to='/petlisting'>Adopt Now</Link></button>
+          </div>
+          <img className="w-full h-[80vh] object-cover" src="https://i.ibb.co/kqK4b72/wepik-export-20231203105027z-BHe.jpg
+
+" alt="" />
         </SwiperSlide>
-        <SwiperSlide>
-            <img className="w-full h-[80vh] object-cover" src="https://i.ibb.co/10pq6F2/images-1.jpg" alt="" />
+        <SwiperSlide className="relative">
+          <div className="absolute lg:left-24 text-center lg:top-32  ">
+            <h2 className=" text-4xl font-bold mb-4 text-white "> Add a New Pet To Your Family  <br /> <span className="text-red-400 pt-4">...A Pet</span> </h2>
+            <p className=" text-xl font-semibold text-white "> Find joy, adopt a furry friend  today! <br /> Explore  our  loving pets waiting for <br /> their forever homes. Adopt, don't shop!</p>
+            <button className="mt-6 py-2 px-3 rounded-lg bg-slate-400 text-xl font-semibold "><Link to='/petlisting'>Adopt Now</Link></button>
+          </div>
+          <img className="w-full h-[80vh] object-cover" src="https://i.ibb.co/MB6NjfP/wepik-export-20231203105207-Yd-RP.jpg" alt="" />
         </SwiperSlide>
-        <SwiperSlide>
-            <img className="w-full h-[80vh] object-cover" src="https://i.ibb.co/10pq6F2/images-1.jpg" alt="" />
+        <SwiperSlide className="relative">
+          <div className="absolute lg:left-24 text-center lg:top-32  ">
+            <h2 className=" text-4xl font-bold mb-4 text-white ">Make a Difference: Support us<br /> <span className="text-red-400 pt-4">Cause with Your Generous Donation!</span> </h2>
+            <p className=" text-xl font-semibold text-white "> Empower change with your generosity. Every <br /> donation fuels our mission, creating a brighter future. <br /> Join us in making a difference!!!</p>
+            <button className="mt-6 py-2 px-3 rounded-lg bg-slate-400 text-xl font-semibold "><Link to='/donationCampaign'>Donate Now</Link></button>
+          </div>
+          <img className="w-full h-[80vh] object-cover" src="https://i.ibb.co/sVgj3XD/wepik-export-20231203104401-Ln-IK.jpg" alt="" />
         </SwiperSlide>
-        <SwiperSlide>
-            <img className="w-full h-[80vh] object-cover" src="https://i.ibb.co/10pq6F2/images-1.jpg" alt="" />
-        </SwiperSlide>
-        
+
+
       </Swiper>
-        </div>
-    );
+    </div>
+  );
 };
 
 export default Banner;
