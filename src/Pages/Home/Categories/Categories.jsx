7d6a269117a8946c0f188561797dc893ed5f9625
Jubayer-Ahmed-Sajid <jubayerahmed661ac@gmail.com>
2024-01-02 12:23:46 +0600

    cta section made responsive

--- src/Pages/Home/Categories/Categories.jsx
+++ src/Pages/Home/Categories/Categories.jsx
@@ -5,27 +5,27 @@ const Categories = () => {
     return (
         <div>
             <h2 className=" text-3xl text-center font-bold text my-8">Categories</h2>
-         <div className="grid lg:grid-cols-4 gap-4 mx-4 my-2 lg:text-center">
-         <Link to='categoryPets/cat' className="shadow-lg px-6 py-8">
-                <div className="text-[#6504b5] text-8xl text lg:w-20 mb-4 mx-auto ">
+         <div className="grid mx-12 lg:grid-cols-4 text-center gap-4 lg:mx-4 my-2 lg:text-center">
+         <Link to='categoryPets/cat' className="shadow-lg rounded-md  px-6 py-8">
+                <div className="text-[#6504b5] flex justify-center text-8xl text lg:w-20 mb-4">
                 <FaCat></FaCat>
                 </div>
                 <h2 className="text-2xl font-bold ">Cat</h2>
             </Link>
-         <Link to='categoryPets/dog' className="shadow-lg px-6 py-8 ">
-                <div className="text-[#6504b5] text-8xl  lg:w-20 mb-4 mx-auto ">
+         <Link to='categoryPets/dog' className="shadow-lg rounded-md py-4 px-6 py-8 ">
+                <div className="text-[#6504b5] text-8xl flex justify-center lg:w-20 mb-4 mx-auto ">
                 <FaDog></FaDog>
                 </div>
                 <h2 className="text-2xl font-bold ">Dog</h2>
             </Link>
-         <Link to='categoryPets/fish' className="shadow-lg px-6 py-8 ">
-                <div className="text-[#6504b5] text-8xl text lg:w-20 mb-4 mx-auto ">
+         <Link to='categoryPets/fish' className="shadow-lg rounded-md px-6 py-8 ">
+                <div className="text-[#6504b5] flex justify-center text-8xl text lg:w-20 mb-4 mx-auto ">
                 <FaFish></FaFish>
                 </div>
                 <h2 className="text-2xl font-bold ">Fish</h2>
             </Link>
-         <Link to='categoryPets/bird' className="shadow-lg px-6 py-8 text">
-                <div className="text-[#6504b5] text-8xl  lg:w-20 mb-4 mx-auto ">
+         <Link to='categoryPets/bird' className="shadow-lg rounded-md px-6 py-8 text">
+                <div className="text-[#6504b5] flex justify-center text-8xl  lg:w-20 mb-4 mx-auto ">
                 <FaKiwiBird></FaKiwiBird>
                 </div>
                 <h2 className="text-2xl font-bold ">Bird</h2>
